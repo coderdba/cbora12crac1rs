dbbb08fa7533aff4376618f4c482470026b28de6
Coder <coderdba@users.noreply.github.com>
2018-05-30 12:31:54 +0530

    Update configure_oracleasm.rb

--- recipes/configure_oracleasm.rb
+++ recipes/configure_oracleasm.rb
@@ -12,7 +12,7 @@ bash 'configuring oracleasm driver on node1' do
   user "root"
   code <<-EOH
     #oracleasm configure -e -u grid -g oinstall -s y  > /tmp/oracleasm.configure.node1.out
-    oracleasm configure -e -u grid -g asmadmin -s y  > /tmp/oracleasm.configure.node1.out
+    oracleasm configure -e -u grid -g asmdba -s y  > /tmp/oracleasm.configure.node1.out
     oracleasm init >> /tmp/oracleasm.configure.node1.out
   EOH
 end
@@ -21,7 +21,7 @@ bash 'configuring oracleasm driver on node2' do
   user "root"
   code <<-EOH
     #sshpass -p "#{node[:rootPassword]}" ssh -o StrictHostKeyChecking=no "root"@"#{node[:hostnameNode2]}" oracleasm configure -e -u grid -g oinstall -s y  > /tmp/oracleasm.configure.node2.out
-    sshpass -p "#{node[:rootPassword]}" ssh -o StrictHostKeyChecking=no "root"@"#{node[:hostnameNode2]}" oracleasm configure -e -u grid -g asmadmin -s y  > /tmp/oracleasm.configure.node2.out
+    sshpass -p "#{node[:rootPassword]}" ssh -o StrictHostKeyChecking=no "root"@"#{node[:hostnameNode2]}" oracleasm configure -e -u grid -g asmdba -s y  > /tmp/oracleasm.configure.node2.out
 
     sshpass -p "#{node[:rootPassword]}" ssh -o StrictHostKeyChecking=no "root"@"#{node[:hostnameNode2]}" oracleasm init >> /tmp/oracleasm.configure.node2.out
 
